db842df54c229ba1e88ccfe64024b4ecbe74920a
enzodqr <darioqr11@gmail.com>
2019-08-20 11:57:10 -0600

    Primer commit

---
+++ app/src/main/java/com/app/medical/Auth/SingUp_Activity.java
@@ -0,0 +1,16 @@
+package com.app.medical.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.medical.R;
+
+public class SingUp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
