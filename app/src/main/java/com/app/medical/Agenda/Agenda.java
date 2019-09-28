a197e3d02309a762362ba6ef5868a8b2856d573f
Kvn1996 <kepove1996@hotmail.com>
2019-09-28 17:26:30 -0600

    cambio de icono

--- app/src/main/java/com/app/medical/Agenda/Agenda.java
+++ app/src/main/java/com/app/medical/Agenda/Agenda.java
@@ -16,49 +16,11 @@ import com.app.medical.R;
 
 public class Agenda extends AppCompatActivity {
 
-    Button boton;
-    //para el modal de editar
-    Dialog pop_up;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
 
-        //para el modal editar
-        pop_up = new Dialog(this);
-
-        boton = (Button) findViewById(R.id.btn_prueba_popUp);
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                show_modal(view);
-            }
-        });
     }
 
-    public void show_modal(View v){
-        TextView cerrar;
-        ImageButton btn_cancel;
-
-        pop_up.setContentView(R.layout.edit_popup);
-        cerrar = (TextView) pop_up.findViewById(R.id.txt_cerrar_popup);
-        cerrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pop_up.dismiss();
-            }
-        });
-        pop_up.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        pop_up.show();
-
-        btn_cancel = (ImageButton) pop_up.findViewById(R.id.btn_cancelEdit);
-        btn_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pop_up.dismiss();
-            }
-        });
-
-    }
 }
