9c89ddb804c716cdbc84b866f43bcdce2fae5403
Kvn1996 <kepove1996@hotmail.com>
2019-09-28 20:54:46 -0600

    calendario parte 1 con fallos

--- app/src/main/java/com/app/medical/Agenda/Agenda.java
+++ app/src/main/java/com/app/medical/Agenda/Agenda.java
@@ -1,26 +1,92 @@
 package com.app.medical.Agenda;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.medical.R;
+import com.google.api.Distribution;
 
-public class Agenda extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agenda);
+public class Agenda extends LinearLayout {
 
+    ImageButton next, prev; // para paginación de meses
+    TextView currentDate; //pfecha actual
+    GridView gridview; //vista del calendario
+    private static final int MAX_CALENDAR_DAYS = 42;
+    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    Context context;
+    /*formatos de las fechas*/
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMM yyyy",Locale.ENGLISH);
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MMM", Locale.ENGLISH);
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+    List<Date> dates = new ArrayList<>();
+    List<Events> eventsList = new ArrayList<>();
+
+
+    public Agenda(Context context) {
+        super(context);
     }
+
+    public Agenda(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        initializeLayout();
+        prev.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar.add(Calendar.MONTH, -1);
+                setUpCalendar();
+            }
+        });
+
+        next.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar.add(Calendar.MONTH, +1);
+                setUpCalendar();
+            }
+        });
+    }
+
+    public Agenda(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void initializeLayout(){
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_agenda, this);
+        next = view.findViewById(R.id.next_btn);
+        prev = view.findViewById(R.id.previous_btn);
+        currentDate = findViewById(R.id.txt_curDate);
+        gridview = findViewById(R.id.gridview);
+    }
+
+    private void setUpCalendar(){
+        String current_date = dateFormat.format(calendar.getTime());
+        currentDate.setText(current_date);
+    }
+
+
 
 }
