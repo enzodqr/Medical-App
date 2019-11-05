48add580a862e7f1de03e97f9c211dbe394f3246
enzodqr <darioqr11@gmail.com>
2019-11-05 11:24:45 -0600

    Medicinas

--- app/src/main/java/com/app/medical/Medicinas/Medicinas.java
+++ app/src/main/java/com/app/medical/Medicinas/Medicinas.java
@@ -3,30 +3,52 @@ package com.app.medical.Medicinas;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.app.medical.DB_Utilities.DB_Utilities;
 import com.app.medical.Menu.Menu_Activity;
+import com.app.medical.Profile.Profile_Activity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.medical.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Medicinas extends AppCompatActivity {
 
     ArrayList<Medicina_model> listDatos;
     RecyclerView recycler;
-
     ImageButton back_btn;
+
+    TextView medicine_msg;
+
+    Medicina_Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +61,8 @@ public class Medicinas extends AppCompatActivity {
 
         back_btn = findViewById(R.id.medicine_back);
 
+        medicine_msg =findViewById(R.id.medicine_msg);
+
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,41 +70,19 @@ public class Medicinas extends AppCompatActivity {
             }
         });
 
-        rellenarLista();
+        check_data();
 
-        Medicina_Adapter adapter = new Medicina_Adapter(listDatos);
-
-        adapter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                go_to_medicine();
-                Toast.makeText(Medicinas.this, "Presionado: "+listDatos.get(recycler.getChildAdapterPosition(view)).getNombre()+" ", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        recycler.setAdapter(adapter);
-
+        get_data();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                add_medicine();
             }
         });
     }
 
-    private void rellenarLista() {
-        listDatos.add(new Medicina_model("Medicina 1","Dosis 1",R.drawable.baseline_account_circle_white_18dp));
-        listDatos.add(new Medicina_model("Medicina 2","Dosis 2",R.drawable.baseline_account_circle_white_18dp));
-        listDatos.add(new Medicina_model("Medicina 3","Dosis 3",R.drawable.baseline_account_circle_white_18dp));
-        listDatos.add(new Medicina_model("Medicina 4","Dosis 4",R.drawable.baseline_account_circle_white_18dp));
-        listDatos.add(new Medicina_model("Medicina 5","Dosis 5",R.drawable.baseline_account_circle_white_18dp));
-        listDatos.add(new Medicina_model("Medicina 6","Dosis 6",R.drawable.baseline_account_circle_white_18dp));
-        listDatos.add(new Medicina_model("Medicina 7","Dosis 7",R.drawable.baseline_account_circle_white_18dp));
-        listDatos.add(new Medicina_model("Medicina 8","Dosis 8",R.drawable.baseline_account_circle_white_18dp));
-    }
 
     private void go_to_medicine(){
         Intent intent = new Intent(getApplicationContext(), MedicineInfo.class);
@@ -105,5 +107,82 @@ public class Medicinas extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
+
+
+    public void show_msg(boolean flag){
+
+        if(flag){
+            medicine_msg.setVisibility(View.VISIBLE);
+        } else {
+            medicine_msg.setVisibility(View.GONE);
+        }
+
+    }
+
+    public void add_medicine(){
+        Intent intent = new Intent(getApplicationContext(), AddMedicineActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void check_data(){
+
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        CollectionReference collectionReference = firestore.
+                collection(DB_Utilities.MEDICINE + auth.getUid());
+
+        collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if(queryDocumentSnapshots.size() != 0){
+
+                    Log.d("Medicine", "Collection exists!");
+                    show_msg(false);
+                } else {
+                    show_msg(true);
+                }
+            }
+        });
+
+    }
+
+    private void get_data(){
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        CollectionReference collectionReference = firestore.
+                collection(DB_Utilities.MEDICINE + auth.getUid());
+
+        collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    List<String> list = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        list.add(document.getId());
+                        listDatos.add(new Medicina_model(document.get("medicina").toString(),document.get("dosis").toString(),R.drawable.baseline_account_circle_white_18dp));
+
+                    }
+                    adapter = new Medicina_Adapter(listDatos);
+                    adapter.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(getApplicationContext(), MedicineInfo.class);
+                            intent.putExtra("Medicina", listDatos.get(recycler.getChildAdapterPosition(view)).getNombre());
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                    recycler.setAdapter(adapter);
+                    Log.d("Medicine", listDatos.toString());
+                } else {
+                    Log.d("Medicine", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+    }
+
 
 }
--- app/src/main/java/com/app/medical/Medicinas/MedicineInfo.java
+++ app/src/main/java/com/app/medical/Medicinas/MedicineInfo.java
@@ -1,47 +1,91 @@
 package com.app.medical.Medicinas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.app.medical.DB_Utilities.DB_Utilities;
 import com.app.medical.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedicineInfo extends AppCompatActivity {
 
     ImageButton back_btn;
+    String nombre_medicina;
+    Intent intent;
+
+    TextView medicina, dosis, periodo, uso, fecha, presentacion;
+    Button editar, borrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine_info);
 
-        back_btn = findViewById(R.id.info_medicine_back);
+        medicina = findViewById(R.id.info_med_titulo);
+        dosis = findViewById(R.id.info_med_dosis);
+        periodo = findViewById(R.id.info_med_periodo);
+        uso = findViewById(R.id.info_med_uso);
+        presentacion =  findViewById(R.id.info_med_presentacion);
+        fecha = findViewById(R.id.info_med_fecha);
+        editar = findViewById(R.id.info_med_editar);
+        borrar = findViewById(R.id.info_med_borrar);
 
-        back_btn.setOnClickListener(new View.OnClickListener() {
+        intent = getIntent();
+        nombre_medicina = intent.getExtras().getString("Medicina");
+        Log.d("Medicina", nombre_medicina);
+
+        get_data();
+
+    }
+
+    private void get_data(){
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        DocumentReference documentReference = firestore.
+                collection(DB_Utilities.MEDICINE + auth.getUid()).document(nombre_medicina);
+
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View view) {
-                go_to_medicine();
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    List<String> list = new ArrayList<>();
+                    DocumentSnapshot document = task.getResult();
+                    list.add(document.get(DB_Utilities.MED_MEDICINA).toString());
+                    list.add(document.get(DB_Utilities.MED_DOSIS).toString());
+                    list.add(document.get(DB_Utilities.MED_PERIODO).toString());
+                    list.add(document.get(DB_Utilities.MED_USO).toString());
+                    list.add(document.get(DB_Utilities.MED_PRESENTACION).toString());
+                    list.add(document.get(DB_Utilities.MED_FECHA).toString());
+
+                    Log.d("Medicina", "Lista: "+list.toString());
+
+                } else {
+                    Log.d("Medicina", "Error getting documents: ", task.getException());
+                }
             }
         });
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_BACK){
-            go_to_medicine();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-
-    private void go_to_medicine(){
-        Intent intent = new Intent(getApplicationContext(), Medicinas.class);
-        startActivity(intent);
-        finish();
     }
 }
